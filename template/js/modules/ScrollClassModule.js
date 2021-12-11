5b537bb7d7c2ee55ad082a8c7cf03b2b7c9ed880
thành <vinhthanhluong@gmail.com>
2021-12-12 00:45:00 +0700

    update js class

--- template/js/modules/ScrollClassModule.js
+++ template/js/modules/ScrollClassModule.js
@@ -1,9 +1,9 @@
 export default function ScrollClassModule() {
-    if ($('.main-content-landing').length) {
+    if ($('.header').length) {
         var sections = $('.landing')
             , header = $('.header')
-            , header_height = header.outerHeight() - 40
-        , content_list = $('.box-fx .box-list');
+            , header_height = header.outerHeight() - 40;
+        // , content_list = $('.box-fx .box-list');
 
         $(window).on('scroll', function () {
             var cur_pos = $(this).scrollTop();
@@ -14,16 +14,16 @@ export default function ScrollClassModule() {
                     var topnews = parseInt(top),
                     bottomnews = parseInt(bottom);
                 if (cur_pos >= topnews && cur_pos <= bottomnews) {
-                    content_list.find('a').removeClass('active');
+                    header.find('a').removeClass('active');
                     // sections.removeClass('active');
                     // console.log($(this));
                     // $(this).addClass('active');
-                    content_list.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+                    header.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
                 }
             });
         });
 
-        content_list.find('a').on('click', function () {
+        header.find('a').on('click', function () {
             var $el = $(this)
                 , id = $el.attr('href');
 
